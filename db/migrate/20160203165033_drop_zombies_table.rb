0dfd19f8afb89475727a0a04fd896f00e90b0525
Matt Morris <mcm0027@yahoo.com>
2016-02-03 10:56:44 -0600

    generate migrations

---
+++ db/migrate/20160203165033_drop_zombies_table.rb
@@ -0,0 +1,17 @@
+class DropZombiesTable < ActiveRecord::Migration
+  def change
+  end
+
+  def up
+    drop_table :zombies
+  end
+
+  def down
+    create_table  :zombies do |t|
+      t.string    :name
+      t.text      :bio
+      t.integer   :age
+      t.timestamps
+    end
+  end
+end
